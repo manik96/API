3cc4bba0ab27487d214cb7ede6f22bf672221ea3
manik96 <59660548+manik96@users.noreply.github.com>
2021-06-18 18:23:11 -0500

    Archivo de pruebas del ambiente de trabajo

--- API/hello.py
+++ API/hello.py
@@ -1,2 +1,10 @@
 #Archivo de pruebas para corroborar el estado del ambiente de trabajo
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
