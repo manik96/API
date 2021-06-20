8e61ea9599d5a4193be13602ae448478eaa92a03
manik96 <59660548+manik96@users.noreply.github.com>
2021-06-20 17:16:01 -0500

    añadido la funcion de creacion de entradas

--- API/load.py
+++ API/load.py
@@ -12,6 +12,7 @@ def load():
 
         spath = str(path)
         state = File_loader(spath)
+
         if isinstance(state, str):
             error = state
 
@@ -31,3 +32,43 @@ def load():
         print(error)
 
     return render_template('/load.html')
+
+@bp.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        name = request.form['Nombre']
+        lname = request.form['Apellido']
+        nac = request.form['Nacionalidad']
+        date = request.form['Fecha de Contrato']
+        sex = request.form['Sexo']
+        error = None
+
+        if not name:
+            error = 'Nombre es un campo obligatorio'
+        elif not lname:
+            error = 'Apellido es un campo obligatorio'
+        elif not nac:
+            nac = "<NA>"
+        elif not date:
+            error = 'Fecha de contrato es un campo obligatorio'
+        elif not sex:
+            error = 'Sexo es un campo obligatorio'
+
+        if error is not None:
+            flash(error)
+        else:
+            name = str(name)
+            lname = str(lname)
+            nac = str(nac)
+            date = str(date)
+            sex = str(sex)
+            
+            db = get_db()
+            db.execute(
+                'INSERT INTO data (nombre, apellido, nacionalidad, fechaContrato, sexo)'
+                ' VALUES (?, ?, ?, ?, ?)', (name, lname, nac, date, sex)
+            )
+            db.commit()
+            return redirect(url_for('viewer.view'))
+
+    return render_template('/create.html')
