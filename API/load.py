827e8b493573d479c232f5f490ccf76121b1e1f2
manik96 <59660548+manik96@users.noreply.github.com>
2021-06-20 19:23:50 -0500

    creada funcion de actualizacion

--- API/load.py
+++ API/load.py
@@ -47,9 +47,9 @@ def create():
             error = 'Nombre es un campo obligatorio'
         elif not lname:
             error = 'Apellido es un campo obligatorio'
-        elif not nac:
+        if not nac:
             nac = "<NA>"
-        elif not date:
+        if not date:
             error = 'Fecha de contrato es un campo obligatorio'
         elif not sex:
             error = 'Sexo es un campo obligatorio'
@@ -62,7 +62,7 @@ def create():
             nac = str(nac)
             date = str(date)
             sex = str(sex)
-            
+
             db = get_db()
             db.execute(
                 'INSERT INTO data (nombre, apellido, nacionalidad, fechaContrato, sexo)'
@@ -72,3 +72,65 @@ def create():
             return redirect(url_for('viewer.view'))
 
     return render_template('/create.html')
+
+@bp.route('/update', methods=('GET', 'POST'))
+def update():
+    if request.method == 'POST':
+        idn = int(request.form['ID'])
+
+        name = request.form['Nombre']
+        lname = request.form['Apellido']
+        nac = request.form['Nacionalidad']
+        date = request.form['Fecha de Contrato']
+        sex = request.form['Sexo']
+        error = None
+
+        print(type(name))
+
+        if idn is None:
+            error = "Se requiere el campo de ID."
+        elif not isinstance(idn, int) or idn < 0:
+            error = "Solo se aceptan numeros enteros positivos."
+        if name == "":
+            name = "<NA>"
+        if lname == "":
+            lname = "<NA>"
+        if nac == "":
+            nac = "<NA>"
+        if date == "":
+            date = "<NA>"
+        if sex == "":
+            sex = "<NA>"
+
+        if error is not None:
+            flash(error)
+        else:
+            name = str(name)
+            lname = str(lname)
+            nac = str(nac)
+            date = str(date)
+            sex = str(sex)
+
+            db = get_db()
+
+            number = db.execute('SELECT MAX(id) FROM data').fetchone()
+            number = number[0]
+
+            if idn > number:
+                flash("El numero provisto esta fuera del rango de la base de datos")
+                print("Error en el indice de busqueda")
+                print(idn)
+                print(number)
+                return render_template('/update.html')
+            
+            db.execute(
+                'UPDATE data'
+                ' SET nombre=?, apellido=?, nacionalidad=?, fechaContrato=?, sexo=?'
+                ' WHERE id=?',(name, lname, nac, date, sex, idn)
+            )
+            db.commit()
+
+            return redirect(url_for('viewer.view'))
+
+
+    return render_template('/update.html')
